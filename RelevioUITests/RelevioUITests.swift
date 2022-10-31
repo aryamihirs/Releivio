03f83dc852113ebb201947b082d85d50c4851b53
Arya Mihir Singh <s.aryamihir@gmail.com>
2022-10-31 09:53:35 -0700

    Adding tests
    Adding Tests
    
    Co-Authored-By: meihuij <108645889+meihuij@users.noreply.github.com>

--- RelevioUITests/RelevioUITests.swift
+++ RelevioUITests/RelevioUITests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import SwiftUI
 
 final class RelevioUITests: XCTestCase {
 
@@ -28,6 +29,41 @@ final class RelevioUITests: XCTestCase {
         app.launch()
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(true, "Execution test passed!")
+    }
+    
+    
+    func testRetreveStressLevelColor(stressLevel: Int, color: Color){
+        let app = XCUIApplication()
+        app.launch()
+        Bool res = getStressLevelColor(stressLevel) == color
+        XCTAssert(res, "Execution test passed!")
+    }
+
+    func testRetreveStressLevelEmojis(Int: stressLevel, emoji){
+        let app = XCUIApplication()
+        app.launch()
+        res = getStressLevelEmoji(stressLevel) == emoji
+        XCTAssert(res, "Execution test passed!")
+    }
+
+    func testChangeInColorBasedOnStressLevel(stressLevel, color){
+        let app = XCUIApplication()
+        app.launch()
+        prev = getStressLevelColor(stressLevel)
+        curr = getStressLevelColor((stressLevel+50)//100)
+        res = prev != curr
+        XCTAssert(res, "Execution test passed!")
+    }
+
+    func testRetreveStressLevelValue(){
+        let app = XCUIApplication()
+        app.launch()
+        val = getRandomStressLevelValue()
+        if val<101 and val>0:
+            res = True
+        else:
+            res = False
+        XCTAssert(res, "Execution test passed!")
     }
     
     func testChangeInColorBasedOnStressLevel(){}
